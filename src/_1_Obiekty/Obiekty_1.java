666869e2df1720385ff797efc158d924a8fd31fc
kamil7745 <kamil7745@gmail.com>
2018-05-24 20:51:11 +0200

    OBIEKTY 2

--- src/_1_Obiekty/Obiekty_1.java
+++ src/_1_Obiekty/Obiekty_1.java
@@ -1,4 +1,24 @@
 package _1_Obiekty;
 
 public class Obiekty_1 {
+
+    public static void main(String[] args) {
+        //----KLASY METODY I OBIEKTY-----
+        /*
+        metody i pola static sa zwiazane z klasa a nie jej obiektem
+        Obiekt jest naturalna reprezentacja klasy, przechowuje informacje
+         o zmiennych oraz mozemy sie do nich odwolac, mozemy za pomoca
+         obiektow wywolac metody jej klasy
+
+        */
+        Punkt punkt1 = new Punkt();
+        punkt1.wspX = 10;
+        punkt1.wspY = 20;
+        System.out.println("Wspolrzedne punku to: "+punkt1.wspX+" "+punkt1.wspY);
+    }
+}
+
+class Punkt{
+    int wspX;
+    int wspY;
 }
