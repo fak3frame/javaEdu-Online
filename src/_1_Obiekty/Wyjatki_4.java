e0dd6ee15593e878ba3ce454a89a695418ebc66d
fak3frame <kamil7745@gmail.com>
2018-05-31 15:14:03 +0200

    Wyjatki 4_4

--- src/_1_Obiekty/Wyjatki_4.java
+++ src/_1_Obiekty/Wyjatki_4.java
@@ -1,6 +1,8 @@
 package _1_Obiekty;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Wyjatki_4 {
     public static void main(String[] args) {
@@ -25,6 +27,7 @@ public class Wyjatki_4 {
 
           Hierarchia dziedziczenia wyjątków Throwable <- Exception <- RuntimeException <-
           <- IllegalArgumentException
+
           W przypadku gdy wyjatek dziedziczy po Exception a nie ma RuntimeException jest
            wyjatkiem checked, w kazdym innym przypadku jest typu unchecked
           Wyjatki checked uzywmy gdy chcemy miec mozliwosc naprwienia bledy programie
@@ -39,8 +42,43 @@ public class Wyjatki_4 {
         catch (IOException e){
             e.printStackTrace(); //metoda pokazuje stos wywolania
         }
+        finally {
+            System.out.println("to sie wykona zawsze");
+        }
+        /*
+        Dobre praktyki:
+        -blok try powinien byc jak najmniejszy - dobra czytelnosc
+        -blok finally jest czesto niezbedny gdy operujemy na obiektach
+        -nalezy uzywac dopasowanych klas wyjatkow do sytouacji lub stworzyc wlasba
+         klase wyjatku
+        -podawac szczegolowy opis przechwyconego wyjatku
+        */
 
+        //PRZYKLAD:
+        System.out.println("Podaj liczbę: ");
+        Scanner wczytaj = new Scanner(System.in);
+        double mojaLiczba = 0;
+        while (true) {
+            try {
+                mojaLiczba = wczytaj.nextDouble();
+                break;//wyjdzie z while
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Podaj poprawną liczbę!");
+                // ignoring wrong token
+                wczytaj.next();
+            }
+        }
+
+        if (mojaLiczba < 0) {
+            throw new IllegalArgumentException(String.format("Pierwiastek kwadratowy" +
+                    " z liczby rzeczywistej %.4f nie istnieje!", mojaLiczba));
+            //komenda wyrzuci wyjatek i pokaz odpwoeidni komunikat
+        }
+
+        System.out.print(String.format("Pierwieastek z %.4f to %.4f.", mojaLiczba, Math.sqrt(mojaLiczba)));
     }
+
 
     public int pokazLiczbeSekund(int godziny){
         if(godziny < 0){
@@ -50,7 +88,8 @@ public class Wyjatki_4 {
         return godziny*60*60;
     }
     public void jakasMetodaZCheckedException()throws IOException{
-        //ta metoda wyrzuca wyjatek checked czyli taki jaki musiby obluzyc
+        //ta metoda MOZE dac wyjatek checked czyli taki jaki musiby obsluzyc
+        //nie obsluguje go wewnatrz
         throw new IOException();
     }
 }
