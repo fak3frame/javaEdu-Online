1ac41936bc6a1452a21d2e7632ec2e271339371d
kamil7745 <kamil7745@gmail.com>
2018-05-31 18:16:08 +0200

    Listy 2

--- src/PrzydatneKlasy/InterfaceList.java
+++ src/PrzydatneKlasy/InterfaceList.java
@@ -1,4 +1,14 @@
 package PrzydatneKlasy;
 
 public class InterfaceList {
+    public static void main(String[] args) {
+        /*
+        Jedna ze struktur danych sa listy w ktorych wyrozniamy najlpopularniejsze
+         ArrayList - implemenacja tablicowa oraz LinkedList -/- dowiazana
+        ArrayList uzywamy gdy mamy czesty dostep do elementow natomiast
+         LikedListy gdy bedziemy czesto wynonywac operacje takie jak dodawanie
+         usuwanie elementow itp
+
+        */
+    }
 }
