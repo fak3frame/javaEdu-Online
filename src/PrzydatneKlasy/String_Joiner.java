2d81206bc93e84d8ee3ffd6d7d460a1d0d5e0c8a
fak3frame <kamil7745@gmail.com>
2018-06-05 23:34:37 +0200

    Przydatne Klasy StringJoiner 4

---
+++ src/PrzydatneKlasy/String_Joiner.java
@@ -0,0 +1,17 @@
+package PrzydatneKlasy;
+
+import java.util.StringJoiner;
+
+public class String_Joiner {
+    public static void main(String[] args) {
+        /*
+        StringJoiner rozwiazuje problem ze StringBuilder/Buffer koliduje
+        z wyrazeniami lambda oraz programowniem funckjnym
+
+        StringJoiner upraszcza laczenie Stringow
+         */
+        StringJoiner joiner = new StringJoiner("");
+        String napis;
+
+    }
+}
