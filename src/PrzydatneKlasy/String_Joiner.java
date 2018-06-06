c112d46b8676061cc8fbe05ce5421d6f6130f73e
kamil7745 <kamil7745@gmail.com>
2018-06-06 19:53:13 +0200

    Przydatne Klasy StringJoiner 11

--- src/PrzydatneKlasy/String_Joiner.java
+++ src/PrzydatneKlasy/String_Joiner.java
@@ -11,19 +11,25 @@ public class String_Joiner {
         StringJoiner upraszcza laczenie Stringow
          */
         StringJoiner joiner = new StringJoiner("");
-        //musi byc cos w konstruktorze StringJoinera
+        //Konstruktor StringJoiner przyjmuje albo jeden parametr
+        // będący separatorem, albo trzy argumenty (separator, prefix, sufix)
 
         String napis = joiner.add("napis").add(" napis2").toString();
         //operacje dodawania metoda .add("")
         //moge przypisac do Stringa uzywajac na koncu metody .toString();
+        System.out.println(napis);
 
         /*
         StringJoiner przyda się szczególnie wtedy, gdy będziemy chcieli
          połączyć kilka napisów i dodatkowo rozdzielić je jakimś separatorem
 
-
-
          */
+        System.out.println();
+
+
+        StringJoiner joiner2 = new StringJoiner(",", "{", "}");
+        String result = joiner2.add("Ania").add("ma").add("kota").toString();
+        System.out.println(result);
 
     }
 }
