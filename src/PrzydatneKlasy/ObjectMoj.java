97b6dd6887bd5acaef3f585ccd730b9935677871
fak3frame <kamil7745@gmail.com>
2018-06-06 22:19:11 +0200

    Przydatne Klasy Objects 2

--- src/PrzydatneKlasy/ObjectMoj.java
+++ src/PrzydatneKlasy/ObjectMoj.java
@@ -2,6 +2,11 @@ package PrzydatneKlasy;
 
 public class ObjectMoj {
     public static void main(String[] args) {
+        /*
+        Jednymi z najwazniejszych funkcji klasy Object jest
+         sprawdzanie czy obiek jest nullem, wywolywanie metody
+         toString() czy equals() w bezpieczny spsob
 
+         */
     }
 }
