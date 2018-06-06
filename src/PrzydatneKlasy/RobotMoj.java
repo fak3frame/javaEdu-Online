233ff0e6f60980bcee50b7516c63c7eaffa84498
kamil7745 <kamil7745@gmail.com>
2018-06-06 20:25:54 +0200

    Przydatne Klasy Robot 1

---
+++ src/PrzydatneKlasy/RobotMoj.java
@@ -0,0 +1,20 @@
+package PrzydatneKlasy;
+
+public class RobotMoj {
+    public static void main(String[] args) {
+        /*
+        klasa robot sluzy do automatycznego uzycia klawiatury,
+         kursora oraz robienia zrzutow ekranu i odczytywaniu informacji
+         o pikselach.
+
+        najwazniejsze metody:
+        keyPress(int keycode), keyRelease(int keycode)
+        mousePress(int buttons), mouseRelease(int buttons) - jako
+         argumenty podajemy stałe zdefiniowane w klasie InputEvent
+         (BUTTON1_MASK, BUTTON2_MASK, BUTTON3_MASK)
+        mouseMove(int x, int y)
+        createScreenCapture(Rectangle screenRect)
+        delay(int time) - wstrzymanie wykonywanego wątku w ms.
+         */
+    }
+}
