e2e14226dba86ad01e76b495602e23e04da4d952
fak3frame <kamil7745@gmail.com>
2018-06-06 22:22:17 +0200

    Przydatne Klasy Objects 3

--- src/PrzydatneKlasy/ObjectMoj.java
+++ src/PrzydatneKlasy/ObjectMoj.java
@@ -6,6 +6,15 @@ public class ObjectMoj {
         Jednymi z najwazniejszych funkcji klasy Object jest
          sprawdzanie czy obiek jest nullem, wywolywanie metody
          toString() czy equals() w bezpieczny spsob
+         inne metody:
+         deepEquals()
+         compare()
+         hashCode()
+
+        aby zabezpieczyc sie przed nulem w metodzie toString nalezy
+         wywolac ja z klasy Object ktora zwroci nam null a nie wyjatek
+         nullPointer
+         Objects.toString();
 
          */
     }
