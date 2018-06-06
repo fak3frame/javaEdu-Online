559204957140cf8fdf57c16e70f33e7d6d6bb96d
kamil7745 <kamil7745@gmail.com>
2018-06-06 19:55:46 +0200

    Przydatne Klasy StringJoiner 13

--- src/PrzydatneKlasy/String_Joiner.java
+++ src/PrzydatneKlasy/String_Joiner.java
@@ -16,23 +16,24 @@ public class String_Joiner {
         //separator - to co oddziela nam napsy - podalem " " czyli nic spacja
         //prefix - to co bedzie na poczatku ciagu
         //sufix - na koncu ciagu
-
         String napis = joiner.add("napis").add("napis2").toString();
         //operacje dodawania metoda .add("")
         //moge przypisac do Stringa uzywajac na koncu metody .toString();
         System.out.println(napis);
 
+
         /*
         StringJoiner przyda się szczególnie wtedy, gdy będziemy chcieli
          połączyć kilka napisów i dodatkowo rozdzielić je jakimś separatorem
-
          */
         System.out.println();
-
-
         StringJoiner joiner2 = new StringJoiner(",", "{", "}");
         String result = joiner2.add("Ania").add("ma").add("kota").toString();
         System.out.println(result);
+
+
+
+
 
     }
 }
