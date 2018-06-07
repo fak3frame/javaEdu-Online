d1d67bb6a79e71a7becf44c098b346b2c84da637
kamil7745 <kamil7745@gmail.com>
2018-06-07 15:33:46 +0200

    Przydatne Klasy InterfaceSet 4

--- src/PrzydatneKlasy/InterfaceSet.java
+++ src/PrzydatneKlasy/InterfaceSet.java
@@ -1,5 +1,8 @@
 package PrzydatneKlasy;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class InterfaceSet {
     public static void main(String[] args) {
         /*
@@ -33,8 +36,33 @@ public class InterfaceSet {
         Najważniejsze metody w interfejsie Set to odziedziczone z
          interfejsu Collection:
 
+        boolean add(E e) - dodaje unikalny element do zbioru, w przypadku,
+         gdy element się powtarza dodawanie jest ignorowane. Unikalność jest
+         sprawdzana na podstawie metod hashCode() i equals(). Do zbioru można
+         dodać jedną wartość null
+        boolean contains(Object o) - sprawdza, czy obiekt 'o' znajduje się w zbiorze
+        boolean isEmpty() - sprawdza, czy zbiór jest pusty
+        Iterator iterator() - zwraca iterator. Kolejność elementów jest zależna od
+         tego z jakiej implementacji korzystamy
+        boolean remove(Object o) - usuwa element 'o' ze zbioru
+        int size() - zwraca rozmiar (ilość elementów) zbioru
+
+        W TreeSet mam metodę first(), która zwraca "najmniejszy" obiekt w rozumieniu
+         naturalnego porządku, oraz last(), która zwraca największy z nich
 
          */
+        Set<String> zbior = new TreeSet<>();
+        zbior.add("Kamil");
+        zbior.add("Tomek");
+        zbior.add("Tomek");
+        zbior.add("Przemek");
+        zbior.add("Janusz");
+        zbior.add("Janusz");
+
+        int size = zbior.size();
+        System.out.println(size);
+
+
 
 
     }
