686e26f339c328a64c6993a60c062b831258d31a
kamil7745 <kamil7745@gmail.com>
2018-06-07 15:15:57 +0200

    Przydatne Klasy Math 3

--- src/PrzydatneKlasy/MathMoj.java
+++ src/PrzydatneKlasy/MathMoj.java
@@ -7,7 +7,25 @@ public class MathMoj {
         int a = 10;
         double b = 15.5;
 
+        System.out.println("-1-");
         System.out.println(Math.min(a,b));
+
+        System.out.println("-2-");
         System.out.println(Math.max(a,b));
+
+        System.out.println("-3-");
+        System.out.println(Math.abs(b));
+
+        System.out.println("-4-");
+        System.out.println(Math.pow(a,2));
+
+        System.out.println("-5-");
+        System.out.println(Math.sqrt(a));
+
+        System.out.println("-6-");
+        System.out.println(Math.round(b)); //zaokraglenie 0.5+ 0.4-
+
+
+
     }
 }
