2f149ff6a675dbfe7ac31e7f17e6db847e18da65
kamil7745 <kamil7745@gmail.com>
2018-06-07 15:18:02 +0200

    Przydatne Klasy InterfaceSet 2

--- src/PrzydatneKlasy/InterfaceSet.java
+++ src/PrzydatneKlasy/InterfaceSet.java
@@ -2,6 +2,14 @@ package PrzydatneKlasy;
 
 public class InterfaceSet {
     public static void main(String[] args) {
+        /*
+        Set reprezentuje kolekcje unikalnych obiektow ale
+         bez okreslenia ich kolejnosci poniewaz zalezy to
+         juz od konkretnej implementacji
+
+
+         */
+
 
     }
 }
