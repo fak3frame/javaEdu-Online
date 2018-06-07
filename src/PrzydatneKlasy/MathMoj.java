8e1589ce4b4d52c629599588eabfa154fab81209
kamil7745 <kamil7745@gmail.com>
2018-06-07 15:16:31 +0200

    Przydatne Klasy Math 4

--- src/PrzydatneKlasy/MathMoj.java
+++ src/PrzydatneKlasy/MathMoj.java
@@ -20,7 +20,7 @@ public class MathMoj {
         System.out.println(Math.pow(a,2));
 
         System.out.println("-5-");
-        System.out.println(Math.sqrt(a));
+        System.out.println(Math.sqrt(4));
 
         System.out.println("-6-");
         System.out.println(Math.round(b)); //zaokraglenie 0.5+ 0.4-
