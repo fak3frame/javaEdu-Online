52af9c0476cce8c97fa8e7c80d074797dc7ad3af
kamil7745 <kamil7745@gmail.com>
2018-06-07 15:26:29 +0200

    Przydatne Klasy InterfaceSet 3

--- src/PrzydatneKlasy/InterfaceSet.java
+++ src/PrzydatneKlasy/InterfaceSet.java
@@ -7,6 +7,32 @@ public class InterfaceSet {
          bez okreslenia ich kolejnosci poniewaz zalezy to
          juz od konkretnej implementacji
 
+        Set jest typem generycznym przez co okreslamy jaki
+         typ elem. bedzie przechowywal dzieki czemu unikniemy
+         rzutowania
+
+        HashSet klasa zapewniajaca unikalnosc elementow bez
+         gwarancji ich kolejnosci. wewnatrz wykorzystywana jest
+         tablica mieszajaca co wymaga poprawnej implementacji
+         metod hashCode oraz equals()
+
+        TreeSet jest klasa oprocz unikalnosci elementow gwarantuje
+         uporzadkowanie zgodne z "naturalnym porzadkiem"
+        Porzadek ten jest ustalany przez odpowiednia implementacje
+         interfejsu Comparable lub zastosowanie odpowiedniego
+         Comparatora
+
+        LinkedHashSet klasa wykorzystujac tablice mieszajaca oraz
+         liste podwojnie wiazana dzieki czemu zagwarantowana jest
+         unikalnosc elementow oraz kolejnosc w jakiej zostaly dodane.
+
+        HashSet jest podstawową implementacją, która zapewnia jedynie
+         unikalność elementów, TreeSet gwarantuje dodatkowe sortowanie,
+         a LinkedHashSet pamięta  dodatkowo kolejnoś wstawiania.
+
+        Najważniejsze metody w interfejsie Set to odziedziczone z
+         interfejsu Collection:
+
 
          */
 
