86fa8325d7c27f3aa9fa52d36431fa56b99db4e8
kamil7745 <kamil7745@gmail.com>
2018-06-07 15:12:32 +0200

    Przydatne Klasy Math 2

--- src/PrzydatneKlasy/MathMoj.java
+++ src/PrzydatneKlasy/MathMoj.java
@@ -1,7 +1,13 @@
 package PrzydatneKlasy;
+import java.lang.Math;
 
 public class MathMoj {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 15.5;
+
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.max(a,b));
     }
 }
