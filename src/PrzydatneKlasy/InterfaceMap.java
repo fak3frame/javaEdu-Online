aa1f3fe8d962ebfeae20687e15880489333b74a2
kamil7745 <kamil7745@gmail.com>
2018-06-07 15:45:48 +0200

    Przydatne Klasy InterfaceMap 1

---
+++ src/PrzydatneKlasy/InterfaceMap.java
@@ -0,0 +1,13 @@
+package PrzydatneKlasy;
+
+public class InterfaceMap {
+    public static void main(String[] args) {
+        /*
+        Interfejs Map nie implementuje interfejsu Collection
+        Sa w nim przechowywane elementy w postaci klucz-wartosc
+         gdzie oba moga byc dowolnym typem obikeotwym
+        Jako klucz moze byc przypisana tylko unikalna wartosc, gdy
+         dodamy nowa, stara o takiej samej wartosci zostanie nadpisana
+         */
+    }
+}
