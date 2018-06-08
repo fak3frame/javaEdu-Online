995b9e6c787a840c89d0dc09e943cf7f8ba42a65
kamil7745 <kamil7745@gmail.com>
2018-06-08 16:02:17 +0200

    Przydatne Klasy Random 5

--- src/PrzydatneKlasy/RandomMoj.java
+++ src/PrzydatneKlasy/RandomMoj.java
@@ -9,15 +9,29 @@ public class RandomMoj {
 
         //metody:
         boolean losPrawdaFalsz = generator.nextBoolean();
-        double losZmiennoprzecinkowa = generator.nextDouble()*10;
+
+        double losZmiennoprzecinkowa = generator.nextDouble();
         //zwroci liczbe z przedzialu od 0 do 1
-        System.out.println(Math.round(losZmiennoprzecinkowa));
+        //mozna zmienic zakres mnozac przez cos np generator.nextDouble()*10
+        // pokaze wiec liczbe od 0 - 10 z liczbami po przecinkuu
+        //mozna w takim przypadku urzyc rzutowania na int aby pokazalo bez
+        // reszty
+
+        float lozZmiennoprzecinkowa2 = generator.nextFloat();
+        //mniejsza ilosc liczb po ,
+
+
+        int zmiennaZwyklaBadzUjemna = generator.nextInt();
+        //standardowo pokaze liczby ujemne i dodanie z calego zakresu int
+        //mozna zawezic zakres podajac liczbe int w parametrze metody
+        // np.10 generator.nextInt(10) to zawezi losowane liczby od 0 do 9
 
 
 
-        for(int i=0; i<5; i++){
-            losZmiennoprzecinkowa = generator.nextDouble();
-            System.out.println(losZmiennoprzecinkowa);
+
+        for(int i=0; i<50; i++){
+            zmiennaZwyklaBadzUjemna = generator.nextInt();
+            System.out.println(zmiennaZwyklaBadzUjemna);
         }
     }
 }
