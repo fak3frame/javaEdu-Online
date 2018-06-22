62ab15a9b8e3cb73ef7aa7ce50ff0e441dd9d215
kamil7745 <kamil7745@gmail.com>
2018-06-22 12:35:20 +0200

    SanouczekProgramisty Pakiety 1

--- src/SamouczekProgramisty/Wstep.java
+++ src/SamouczekProgramisty/Wstep.java
@@ -28,8 +28,6 @@ public class Wstep {
          aby zaoszczedzic pisania kodu oraz poprawic jego czytelnosc
         Metody nie powinny przyjmowac duzej ilosc argumentow ("code smell")
         Metoda void moze posiadac slowo retrun ale nie moze nic zwracac
-
-
          */
         System.out.println("METODY:");
         int jakisNumer = 123; //zakres +/- 2,147,483,647
@@ -44,10 +42,20 @@ public class Wstep {
         boolean wynik = czyDuza(jakisNumer);
         System.out.println(wynik);
 
+        /*
+        PAKIETY:
+
+        Package - informuje gdzie znajduje sie klasa/interfejs/typ wyliczeniowy
+
+        package SamouczekProgramisty.folder1;
+        plik klasy znajduje sie w folderze "folder1" a on w folderze
+         SamouczekProgramisty
+
+         */
+
 
     }
     static boolean czyDuza(int liczba){
         return liczba>100;
     }
-
 }
