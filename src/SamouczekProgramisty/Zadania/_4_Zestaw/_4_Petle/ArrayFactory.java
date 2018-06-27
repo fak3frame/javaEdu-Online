d72f03bd3923207601c681d6bfa29b5fd7203a65
kamil7745 <kamil7745@gmail.com>
2018-06-27 18:20:08 +0200

    SanouczekProgramisty Zadania podstawowe 6

--- src/SamouczekProgramisty/Zadania/_4_Zestaw/_4_Petle/ArrayFactory.java
+++ src/SamouczekProgramisty/Zadania/_4_Zestaw/_4_Petle/ArrayFactory.java
@@ -1,5 +1,7 @@
 package SamouczekProgramisty.Zadania._4_Zestaw._4_Petle;
 
+import java.util.Arrays;
+
 public class ArrayFactory {
     int x;
     public ArrayFactory(int x){
@@ -51,7 +53,44 @@ public class ArrayFactory {
             sb.append(x%2);
             x/=2;
         }
-        return Integer.parseInt(sb.reverse().toString());
+        return Integer.valueOf(sb.reverse().toString());
+    }
+    public static boolean czyPalindrom(String x){
+        int licznik = x.length()-1;
+        for (int i = 0; i < x.length()/2; i++) {
+            if(x.charAt(i)==x.charAt(licznik))
+                licznik--;
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void liczbyWTablicy(int[] tab){
+        System.out.print("[");
+        for (int i = 0; i < tab.length-1; i++) {
+            System.out.print(tab[i]+", ");
+        }
+        System.out.println(tab[tab.length-1]+"]");
+    }
+    public static void sortujBabelkowo(int[]tab){
+        int licznik = 0;
+        while (licznik<tab.length){
+            System.out.println("licznik STARTOWY="+licznik);
+            for (int i = 0; i<tab.length-licznik-1; i++){
+                System.out.println("i="+i);
+                System.out.println("tab[i]="+tab[i]);
+                System.out.println("tab[i+1]="+tab[i+1]);
+                if(tab[i]>tab[i+1]){
+                    System.out.println("znalazlem wiekszy element");
+                    int tmp = tab[i];
+                    tab[i]=tab[i+1];
+                    tab[i+1]=tmp;
+                    System.out.println("tab["+i+"]="+tab[i]+", tab["+(i+1)+"]="+tab[i+1]);
+                }
+            }
+            licznik++;
+        }
     }
 
     public static void main(String[] args) {
@@ -63,5 +102,12 @@ public class ArrayFactory {
         System.out.println(odwrocNapis2("kamil"));
         System.out.println(odwrocNapis3("kamil"));
         System.out.println(liczbaBinarna(21));
+        System.out.println(czyPalindrom("kauauak"));
+        liczbyWTablicy(new int[]{3,4,5});
+        int[]t = new int[]{2,4,1,3};
+        sortujBabelkowo(t);
+        System.out.println(Arrays.toString(t));
+
+
     }
 }
