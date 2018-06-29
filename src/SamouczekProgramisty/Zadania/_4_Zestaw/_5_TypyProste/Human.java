1ff3c2fdf72968802d0f0277d25da7d769e851cd
fak3frame <kamil7745@gmail.com>
2018-06-29 21:31:33 +0200

    SanouczekProgramisty Zadania podstawowe 33

--- src/SamouczekProgramisty/Zadania/_4_Zestaw/_5_TypyProste/Human.java
+++ src/SamouczekProgramisty/Zadania/_4_Zestaw/_5_TypyProste/Human.java
@@ -39,6 +39,7 @@ public class Human {
         int i = 0;
         while (licznik>0){
             System.out.println(x[i]+" "+x[i+1]);
+            System.out.println(Arrays.toString(x));
             if(x[i]>x[i+1]){
                 int tmp=x[i];
                 x[i]=x[i+1];
@@ -46,6 +47,7 @@ public class Human {
             }
             i++;
             licznik--;
+            System.out.println(Arrays.toString(x));
         }
     }
 
