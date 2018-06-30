97cc1704ca4b57c07b0c541b4ca2182ee99512c7
kamil7745 <kamil7745@gmail.com>
2018-06-30 14:34:22 +0200

    SanouczekProgramisty Zadania podstawowe 46

--- src/SamouczekProgramisty/Zadania/_4_Zestaw/Interface/FigureFactory.java
+++ src/SamouczekProgramisty/Zadania/_4_Zestaw/Interface/FigureFactory.java
@@ -1,4 +1,26 @@
 package SamouczekProgramisty.Zadania._4_Zestaw.Interface;
 
+import java.util.Scanner;
+
 public class FigureFactory {
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[3];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj promien kola:");
+        figures[0] = new Circle(sc.nextInt());
+
+        System.out.println("Podaj 2 długości boków trójkąta prostokątnego (boki przy kącie prostym):");
+        figures[1] = new Triangle(sc.nextInt(),sc.nextInt());
+
+        System.out.println("Podaj długość boków prostokąta:");
+        figures[2] = new Rectangle(sc.nextInt(),sc.nextInt());
+
+        for(Figure x : figures){
+            System.out.println("Pole figury wynosi: "+x.getArea());
+            System.out.println("Obwod figury wynosi: "+x.getPerimeter());
+        }
+
+
+    }
 }
