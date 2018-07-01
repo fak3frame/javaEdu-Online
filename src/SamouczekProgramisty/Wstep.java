aba8e8d9dc3b30d41054847189f7876aeaf24f3d
kamil7745 <kamil7745@gmail.com>
2018-07-01 23:45:00 +0200

    SanouczekProgramisty Typy Generyczne 16

--- src/SamouczekProgramisty/Wstep.java
+++ src/SamouczekProgramisty/Wstep.java
@@ -439,12 +439,10 @@ public class Wstep {
     class Pair<T, S> {
         private T first;
         private S second;
-
         public Pair(T first, S second) {
             this.first = first;
             this.second = second;
         }
-
         public T getFirst() {
             return first;
         }
