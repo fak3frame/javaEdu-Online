eec6c138030973673fda5da1a73ee2d1453ddb21
student <kamil7745@gmail.com>
2018-07-05 18:45:17 +0200

    SanouczekProgramisty Typy Generyczne 103

--- src/SamouczekProgramisty/Test1/X4.java
+++ src/SamouczekProgramisty/Test1/X4.java
@@ -2,5 +2,21 @@ package SamouczekProgramisty.Test1;
 
 public class X4<T extends iX> {
     private T o1;
-    public
+    public X4(T o1){
+        this.o1=o1;
+    }
+    public T getO1() {
+        return o1;
+    }
+    public void setO1(T o1) {
+        this.o1 = o1;
+    }
+
+    public void metoda1(X4<?>o){
+        Object x = o.o1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
