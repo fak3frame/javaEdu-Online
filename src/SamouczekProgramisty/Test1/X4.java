4145254241fb7b40cb8d70a0327b86e8c0ab657b
student <kamil7745@gmail.com>
2018-07-05 19:00:08 +0200

    SanouczekProgramisty Typy Generyczne 109

--- src/SamouczekProgramisty/Test1/X4.java
+++ src/SamouczekProgramisty/Test1/X4.java
@@ -4,7 +4,7 @@ public class X4<T extends iX> {
     private T o1;
     public X4(T o1){
         this.o1=o1;
-        System.out.println(o1.dajNapis());
+        //System.out.println(o1.dajNapis());
     }
     public T getO1() {
         return o1;
@@ -27,5 +27,6 @@ public class X4<T extends iX> {
 
     public static void main(String[] args) {
         X4<?> test = new X4<>(new XimplementsiX());
+        test.metoda2(test);
     }
 }
