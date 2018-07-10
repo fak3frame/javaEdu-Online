f907f07df1a5439bdbc884e2d254c64d4e3c97f5
kamil7745 <kamil7745@gmail.com>
2018-07-10 19:53:26 +0200

    SanouczekProgramisty Serializacja 33

--- src/SamouczekProgramisty/Test1/Serializacja1.java
+++ src/SamouczekProgramisty/Test1/Serializacja1.java
@@ -3,7 +3,7 @@ package SamouczekProgramisty.Test1;
 import java.io.Serializable;
 
 public class Serializacja1 implements Serializable {
-    private static int wiek;
+    private static int wiek = 2222;
     public Serializacja1(int wiek){
         this.wiek = wiek;
     }
--- src/SamouczekProgramisty/Test1/SerializacjaFabryka.java
+++ src/SamouczekProgramisty/Test1/SerializacjaFabryka.java
@@ -1,4 +1,23 @@
 package SamouczekProgramisty.Test1;
 
+import java.io.*;
+
 public class SerializacjaFabryka {
+    public static void main(String[] args) {
+        Serializacja1 s = new Serializacja1(123);
+        System.out.println(s.getWiek());
+        try(ObjectOutputStream strWyj = new ObjectOutputStream(new FileOutputStream("serializacjaTest.bin"))){
+            strWyj.writeObject(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try(ObjectInputStream strWej = new ObjectInputStream(new FileInputStream("serializacjaTest.bin"))){
+            Serializacja1 s2 = (Serializacja1) strWej.readObject();
+            System.out.println(s2.getWiek());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
