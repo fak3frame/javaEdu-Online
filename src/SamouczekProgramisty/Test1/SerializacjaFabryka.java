ae9b84d7cfa705d7621b9a330486fb0947471db1
kamil7745 <kamil7745@gmail.com>
2018-07-10 20:01:57 +0200

    SanouczekProgramisty Serializacja 35

--- src/SamouczekProgramisty/Test1/SerializacjaFabryka.java
+++ src/SamouczekProgramisty/Test1/SerializacjaFabryka.java
@@ -5,7 +5,10 @@ import java.io.*;
 public class SerializacjaFabryka {
     public static void main(String[] args) {
         Serializacja1 s = new Serializacja1();
+        System.out.println(s.wiek);
+        System.out.println("po zmianie");
         s.wiek = 1000;
+        System.out.println(s.wiek);
         try(ObjectOutputStream strWyj = new ObjectOutputStream(new FileOutputStream("serializacjaTest.bin"))){
             strWyj.writeObject(s);
         } catch (IOException e) {
@@ -13,6 +16,7 @@ public class SerializacjaFabryka {
         }
         try(ObjectInputStream strWej = new ObjectInputStream(new FileInputStream("serializacjaTest.bin"))){
             Serializacja1 s2 = (Serializacja1) strWej.readObject();
+            System.out.println("po serializacji");
             System.out.println(s2.wiek);
         } catch (IOException e) {
             e.printStackTrace();
