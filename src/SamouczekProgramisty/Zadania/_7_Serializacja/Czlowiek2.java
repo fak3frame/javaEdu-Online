aab706cf1e505e965fd30ecfc999f3f62e4e129b
kamil7745 <kamil7745@gmail.com>
2018-07-11 23:54:38 +0200

    SanouczekProgramisty Serializacja 66

--- src/SamouczekProgramisty/Zadania/_7_Serializacja/Czlowiek2.java
+++ src/SamouczekProgramisty/Zadania/_7_Serializacja/Czlowiek2.java
@@ -6,7 +6,8 @@ public class Czlowiek2 {
     String imie;
     int wiek;
 
+
     public static void main(String[] args) {
-        System.out.println(Calendar.getInstance().get(Calendar.YEARk));
+        System.out.println(Calendar.getInstance().get(Calendar.YEAR));
     }
 }
