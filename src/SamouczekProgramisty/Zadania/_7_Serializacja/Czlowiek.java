b2d769447c80ded8339796c2b8d938d5d1046eff
kamil7745 <kamil7745@gmail.com>
2018-07-11 23:47:13 +0200

    SanouczekProgramisty Serializacja 61

--- src/SamouczekProgramisty/Zadania/_7_Serializacja/Czlowiek.java
+++ src/SamouczekProgramisty/Zadania/_7_Serializacja/Czlowiek.java
@@ -1,39 +1,26 @@
 package SamouczekProgramisty.Zadania._7_Serializacja;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Czlowiek {
-    List imiona = new LinkedList();
-    String imie;
-    Scanner wprowadz = new Scanner(System.in);
 
-    public void input(){
-        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("imiona.bin"))){
-            System.out.println("Podaj imiona:");
-            do {
-                imie = wprowadz.nextLine();
-                if(!imie.equals("-")){
-                    imiona.add(imie);
-                }
-            }while (!imie.equals("-"));
-            out.writeObject(imiona);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static List<String> input(){
+        List<String> imiona = new ArrayList();
+        String imie;
+        Scanner input = new Scanner(System.in);
+        while (true){
+            imie = input.nextLine();
+            if(imie.equals("-"))
+                break;
+            imiona.add(imie);
         }
-        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("imiona.bin"))){
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return imiona;
     }
+
 
     public static void main(String[] args) {
 
