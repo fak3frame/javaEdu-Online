ef7caf4aca319071345854bc4eb44387fc6b61e0
kamil7745 <kamil7745@gmail.com>
2018-07-11 23:28:43 +0200

    SanouczekProgramisty Serializacja 57

--- src/SamouczekProgramisty/Zadania/_7_Serializacja/Czlowiek.java
+++ src/SamouczekProgramisty/Zadania/_7_Serializacja/Czlowiek.java
@@ -1,4 +1,23 @@
 package SamouczekProgramisty.Zadania._7_Serializacja;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Czlowiek {
+    List imiona = new LinkedList();
+
+    public static void main(String[] args) {
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("imiona.bin"))){
+            System.out.println("Podaj imiona:");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
