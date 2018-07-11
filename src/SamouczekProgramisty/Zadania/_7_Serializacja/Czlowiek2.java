cef36ba652b966db1782e8625cafde95fdc6d966
kamil7745 <kamil7745@gmail.com>
2018-07-12 00:08:10 +0200

    SanouczekProgramisty Serializacja 73

--- src/SamouczekProgramisty/Zadania/_7_Serializacja/Czlowiek2.java
+++ src/SamouczekProgramisty/Zadania/_7_Serializacja/Czlowiek2.java
@@ -1,8 +1,9 @@
 package SamouczekProgramisty.Zadania._7_Serializacja;
 
+import java.io.*;
 import java.util.Calendar;
 
-public class Czlowiek2 {
+public class Czlowiek2 implements Serializable {
     String imie;
     int wiek;
     Czlowiek2(String imie, int wiek){
@@ -10,12 +11,23 @@ public class Czlowiek2 {
         this.wiek = wiek;
     }
 
+    private void writeObject(ObjectOutputStream out)throws IOException{
+        out.defaultWriteObject();
+        int wiekUrodzenia = Calendar.getInstance().get(Calendar.YEAR) - wiek;
+        out.writeInt(wiekUrodzenia);
+    }
 
 
     public static void main(String[] args) {
-        System.out.println(Calendar.getInstance().get(Calendar.YEAR));
         Czlowiek2 c1 = new Czlowiek2("kamil",27);
         Czlowiek2 c2 = new Czlowiek2("tomek",29);
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("wiek.bin"))){
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
