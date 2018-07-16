915a2dff122c2f10b160a95581b00a0bbee88308
kamil7745 <kamil7745@gmail.com>
2018-07-16 13:07:13 +0200

    SanouczekProgramisty Serializacja 132

--- src/toRemember.java
+++ src/toRemember.java
@@ -224,7 +224,7 @@ public class toRemember {
         Integer mojInteger2 = Integer.valueOf("123");
         int mojInt1 = Integer.valueOf(444);
         int mojInt2 = Integer.valueOf("1234");
-        //Metoda value of dziala dla int i string
+        //Metoda valueOf dziala dla int i string
         //Moze zapisac wartosci liczbowe go Integera i int
 
         Integer mojInteger3 = Integer.parseInt("123");
