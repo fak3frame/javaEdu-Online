6fd0c43a4a129dbf4c355e5e97495d960670f546
fak3frame <kamil7745@gmail.com>
2018-07-22 22:19:06 +0200

    SanouczekProgramisty KLASY WEWNETRZNE I ANONIMOWE 14

--- src/SamouczekProgramisty/Zadania/_10_KlasyAnonimowe/AnonymousClasses.java
+++ src/SamouczekProgramisty/Zadania/_10_KlasyAnonimowe/AnonymousClasses.java
@@ -1,15 +1,32 @@
 package SamouczekProgramisty.Zadania._10_KlasyAnonimowe;
 
 public class AnonymousClasses {
-
-
-
     public static class Robot{
         private final GreetingModule greetingModule;
 
         public Robot(GreetingModule greetingModule) {
             this.greetingModule = greetingModule;
         }
+        void seySomething(){
+            greetingModule.seyHello();
+        }
+    }
+
+    public static void main(String[] args) {
+        Robot jan = new Robot(new GreetingModule() {
+            @Override
+            public void seyHello() {
+                System.out.println("dzien dobry");
+            }
+        });
+        Robot jon = new Robot(new GreetingModule() {
+            @Override
+            public void seyHello() {
+                System.out.println("good morning");
+            }
+        });
+        jan.seySomething();
+        jon.seySomething();
     }
 }
 
