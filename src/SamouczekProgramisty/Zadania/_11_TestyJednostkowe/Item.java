303f1955725ac296fd1ed80a1fcfcb9ffa1a318e
kamil7745 <kamil7745@gmail.com>
2018-07-28 23:15:31 +0200

    SanouczekProgramisty Test Driven Development 60

--- src/SamouczekProgramisty/Zadania/_11_TestyJednostkowe/Item.java
+++ src/SamouczekProgramisty/Zadania/_11_TestyJednostkowe/Item.java
@@ -1,7 +1,30 @@
 package SamouczekProgramisty.Zadania._11_TestyJednostkowe;
 
+import java.util.Objects;
+
 public class Item {
     private double price;
     private String name;
 
+    public Item(double price, String name) {
+        if (price <= 0)
+            throw new IllegalArgumentException("Price cant be less than 1");
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price, name)
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj instanceof Item) {
+            Item tmp = (Item) obj;
+            return Double.compare(this.price, tmp.price) == 0
+                    && this.name.equals(tmp.name);
+        } else
+            return false;
+    }
 }
