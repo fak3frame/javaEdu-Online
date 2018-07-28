2954cd5c1d7a8cb7823616cb4c6e270ea8cc827f
kamil7745 <kamil7745@gmail.com>
2018-07-28 22:34:46 +0200

    SanouczekProgramisty Test Driven Development 51

--- src/SamouczekProgramisty/Zadania/_11_TestyJednostkowe/ItemTest.java
+++ src/SamouczekProgramisty/Zadania/_11_TestyJednostkowe/ItemTest.java
@@ -1,7 +1,17 @@
 package SamouczekProgramisty.Zadania._11_TestyJednostkowe;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ItemTest {
+    Item item;
+    @Before
+    private void setUp(){
+        item = new Item(100, "cell Phone");
+    }
+    @Test
+
 
 }
