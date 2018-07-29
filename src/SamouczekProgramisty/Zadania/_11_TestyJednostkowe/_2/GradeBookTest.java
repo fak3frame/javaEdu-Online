97c8bef4f13fbe50d22c47c83393c99d38513650
kamil7745 <kamil7745@gmail.com>
2018-07-29 18:09:36 +0200

    SanouczekProgramisty Test Driven Development 83

--- src/SamouczekProgramisty/Zadania/_11_TestyJednostkowe/_2/GradeBookTest.java
+++ src/SamouczekProgramisty/Zadania/_11_TestyJednostkowe/_2/GradeBookTest.java
@@ -1,16 +1,47 @@
 package SamouczekProgramisty.Zadania._11_TestyJednostkowe._2;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.beans.Transient;
 
 import static org.junit.Assert.*;
 
 public class GradeBookTest {
-    GradeBook gradeBook;
-    Subject subject;
+    private final String SUBJECT = "Math";
+    private final String SUBJECT2 = "Physics";
+
+    GradeBook gradeBookWithSubject;
+
+    @Before
+    public void setUp(){
+        gradeBookWithSubject = new GradeBook();
+        gradeBookWithSubject.addSubject(SUBJECT);
+    }
+
     @Test
-    public void canAddSubject(){
-        gradeBook = new GradeBook(new Subject("Math"));
+    public void canCreateGradeBook(){
+        GradeBook gradeBook = new GradeBook();
         assertNotNull(gradeBook);
+    }
+    @Test
+    public void canAddSubjectToGradeBook(){
+        GradeBook gradeBook = new GradeBook();
+        gradeBook.addSubject(SUBJECT);
+    }
+    @Test
+    public void cantAddExistingSubjectToGradeBook(){
+        try{
+            gradeBookWithSubject.addSubject(SUBJECT);
+            fail();
+        }
+        catch (IllegalArgumentException ex){
+            assertEquals("Subject is in GradeBook", ex.getMessage());
+        }
+    }
+    @Test
+    public void canGetSubjectFromGradeBook(){
+
     }
 
 }
