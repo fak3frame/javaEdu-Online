ed9d92f77ea7b6e4edfafad6f25c4e4b1499410e
kamil7745 <kamil7745@gmail.com>
2018-07-29 13:47:29 +0200

    SanouczekProgramisty Test Driven Development 64

--- src/SamouczekProgramisty/Zadania/_11_TestyJednostkowe/BasketTest.java
+++ src/SamouczekProgramisty/Zadania/_11_TestyJednostkowe/BasketTest.java
@@ -6,7 +6,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BasketTest {
+    @Test
+    public void userHaveToAddMinimOneProductAsAmmountToBasket(){
+        try{
+            new Basket(Item, 0);
+            fail();
+        }
+        catch (IllegalArgumentException ex){
+            assertEquals("Ammount of prduct must be more than 0",ex.getMessage());
+        }
+        try{
+            new Basket(Item, -1);
+            fail();
+        }
+        catch (IllegalArgumentException ex){
+            assertEquals("Ammount of prduct must be more than 0", ex.getMessage());
+        }
+    }
+    @Test
+    public void userCantRemoveAmmountOfProductBiggerThanAmmountInBasket(){
+        try{
 
-
-
+        }
+    }
 }
