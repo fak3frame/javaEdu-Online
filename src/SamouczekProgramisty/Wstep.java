e8b8ba5ca16577aa4b5ea701d83bb7116b762cf3
fak3frame <kamil7745@gmail.com>
2018-08-08 20:41:49 +0200

    SanouczekProgramisty WYRAŻENIA REGULARNE 51

--- src/SamouczekProgramisty/Wstep.java
+++ src/SamouczekProgramisty/Wstep.java
@@ -2804,6 +2804,33 @@ public class Wstep {
         dopasowanieNazwyKsiazki.matches();
         System.out.println(dopasowanieNazwyKsiazki.group(1));
 
+        //ZADANIA
+        /* ZAD 1
+        Sprawdza czy liczba zmiennoprzecinkowa podana przez użytkownika
+         ma poprawny format. Na przykład liczba 123,2341515132135 czy -10
+         są poprawne ale 18-12 czy 123, już nie,
+         */
+        Pattern wzorzecZad1 = Pattern.compile("-?\\d+(,\\d+)?");
+
+
+        /* ZAD 2
+        Sprawdza czy numer domu jest w formacie numer\numer.
+         Poprawnym numerem jest 123\2A, 24B\3 czy 12\5, ale
+         już numer abc\cba nie,
+         */
+        Pattern wzorzecZad2 = Pattern.compile("\\d+[a-zA-Z]?\\\\\\d+[a-zA-Z]?");
+
+        /* ZAD 3
+        Sprawdza czy użytkownik wprowadził poprawną nazwę miasta.
+         Na przykład Wrocław, Zielona Gora czy Bielsko-Biala jest ok,
+         jednak Ptysiow123 już nie. Dla uproszczenia załóżmy, że
+         żadna nazwa miejscowości nie zwiera polskich znaków.\
+        */
+        Pattern wzorzecZad3 = Pattern.compile("[A-Z][a-z]+([ -][A-Z][a-z]+)?");
+
+
+
+
 
         String x = new Object() + " 123";
         System.out.println(x);
