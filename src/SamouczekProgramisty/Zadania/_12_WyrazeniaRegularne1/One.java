41abf382edcfddb636239dd1056a093b269659e7
fak3frame <kamil7745@gmail.com>
2018-08-08 19:52:39 +0200

    SanouczekProgramisty WYRAŻENIA REGULARNE 49

--- src/SamouczekProgramisty/Zadania/_12_WyrazeniaRegularne1/One.java
+++ src/SamouczekProgramisty/Zadania/_12_WyrazeniaRegularne1/One.java
@@ -1,5 +1,6 @@
 package SamouczekProgramisty.Zadania._12_WyrazeniaRegularne1;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class One {
@@ -9,13 +10,15 @@ public class One {
          ma poprawny format. Na przykład liczba 123,2341515132135 czy -10
          są poprawne ale 18-12 czy 123, już nie,
          */
-        Pattern wzorzec1 = Pattern.compile("\\-*\\d+\\,+\\d+");
+        Pattern wzorzecZad1 = Pattern.compile("-?\\d+(,\\d+)?");
+
 
         /* ZAD 2
         Sprawdza czy numer domu jest w formacie numer\numer.
          Poprawnym numerem jest 123\2A, 24B\3 czy 12\5, ale
          już numer abc\cba nie,
          */
+        Pattern wzorzecZad2 = Pattern.compile("");
 
         /* ZAD 3
         Sprawdza czy użytkownik wprowadził poprawną nazwę miasta.
