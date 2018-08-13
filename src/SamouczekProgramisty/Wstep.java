95bf2ad80284c1dc28672136cd44203876c4691b
fak3frame <kamil7745@gmail.com>
2018-08-13 15:55:47 +0200

    Wyrażenia lambda 5

--- src/SamouczekProgramisty/Wstep.java
+++ src/SamouczekProgramisty/Wstep.java
@@ -3295,7 +3295,7 @@ public class Wstep {
         //Java posiada zestaw gotowych interfejsow w pakiecie java.util.function, najwazniejsze
         // z nich to:
 //
-//        -Function<T, R> zawiera metodę apply, która przyjmuje instancję klasy T
+//        -Function<T, R> zawiera metodę R apply(T ob), która przyjmuje instancję klasy T
 //          zwracając instancję klasy R
         Function<String, Integer> zmianaSI = new Function<String, Integer>() {
             @Override
@@ -3306,7 +3306,7 @@ public class Wstep {
         Function<String, Integer> zmianaSILambda = zmiennaString -> Integer.parseInt(zmiennaString);
         int L1 = zmianaSILambda.apply("12");
 
-//        -Consumer<T> zawiera metodę accept, która przyjmuje instancję klasy T
+//        -Consumer<T> zawiera metodę void accept<T ob>, która przyjmuje instancję klasy T
         Consumer<Integer> pokazI = new Consumer<Integer>() {
             @Override
             public void accept(Integer integer) {
@@ -3316,7 +3316,7 @@ public class Wstep {
         Consumer<Integer> pokazILambda = jakasLiczba -> System.out.println("liczba metody accept: "+jakasLiczba);
         pokazILambda.accept(123);
 
-//        -Predicate<T> zawiera metodę test, która przyjmuje instancję klasy T i
+//        -Predicate<T> zawiera metodę boolean test<T ob>, która przyjmuje instancję klasy T i
 //          zwraca flagę. Interfejs ten może posłużyć do zastąpienia interfejsu Checker
         Predicate<Integer> czyParzysta = new Predicate<Integer>() {
             @Override
@@ -3327,7 +3327,7 @@ public class Wstep {
         Predicate<Integer> czyParzystaLambda2 = liczbaDoSprawdzenia -> liczbaDoSprawdzenia % 2 == 0;
         System.out.println("Czy liczba 15 jest parzysta : "+czyParzystaLambda2.test(15));
 
-//        -Supplier<T> zawiera metodę get, która nie przyjmuje żadnych parametrów i
+//        -Supplier<T> zawiera metodę T get(), która nie przyjmuje żadnych parametrów i
 //          zwraca instancję klasy T,
         Supplier<Integer> pokazInteger10 = new Supplier<Integer>() {
             @Override
@@ -3338,7 +3338,12 @@ public class Wstep {
         Supplier<Integer> pokazInteger10Lambda = () -> 10;
         System.out.println("pokaz 10 z lambdy : "+pokazInteger10Lambda.get());
 
+//        -IntSuplier zwraca metode int getAsInt() ktora nie przyjmuje zadnych parametrow i
+//          zwraca liczbe int
+//
+
 //        -UnaryOperator<T> jest specyficznym przypadkiem interfejsu Function.
+//          zwraca metode T apply (T ob)
 //          W tym przypadku typ argumentu i typ zwracany są te same.
         UnaryOperator<Integer> pomnozPrzezDwa = new UnaryOperator<Integer>() {
             @Override
@@ -3346,8 +3351,10 @@ public class Wstep {
                 return null;
             }
         };
-        UnaryOperator<Integer> pomnozPrzezDwaLambda = liczbaDoMnozeniaPrzezDwa -> liczbaDoMnozeniaPrzezDwa*2;
+        UnaryOperator<Integer> pomnozPrzezDwaLambda = liczbaDoMnozenia -> liczbaDoMnozenia*2;
         System.out.println("liczba 14 pomnozona przez 2  w lambdzie: "+pomnozPrzezDwaLambda.apply(14));
+
+        }
 
 
 
