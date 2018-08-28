cc53627acdb49d1f68dc45e1c898979ee7371ef7
fak3frame <kamil7745@gmail.com>
2018-08-29 01:43:40 +0200

    Strumienie 68

--- src/SamouczekProgramisty/AplikacjaDoNauki/KategorieData/Stringi.java
+++ src/SamouczekProgramisty/AplikacjaDoNauki/KategorieData/Stringi.java
@@ -2,7 +2,7 @@ package SamouczekProgramisty.AplikacjaDoNauki.KategorieData;
 
 import SamouczekProgramisty.AplikacjaDoNauki.Kategorie;
 
-import java.util.LinkedHashMap;
+
 import java.util.List;
 import java.util.Map;
 
