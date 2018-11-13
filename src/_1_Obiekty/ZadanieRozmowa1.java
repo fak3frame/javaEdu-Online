cba12110d346734ca8793a280e457587d7d060a1
Fak3Frame <kamil7745@gmail.com>
2018-11-13 16:14:47 +0100

    PODSUMOWANIE 247

---
+++ src/_1_Obiekty/ZadanieRozmowa1.java
@@ -0,0 +1,14 @@
+package _1_Obiekty;
+
+public class ZadanieRozmowa1 {
+
+    public static void liczby(){
+        for(int i = 0; i < 100; i++){
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
