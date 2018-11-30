a8bf2bf5e9890f700921f3142c2e48f80dd82e7b
Fak3Frame <kamil7745@gmail.com>
2018-11-30 21:03:37 +0100

    PODSUMOWANIE 275

--- src/Podsumowanie/Ojciec.java
+++ src/Podsumowanie/Ojciec.java
@@ -11,6 +11,7 @@ class Ojciec{
     public static void main(String[] args) {
         Random random = new Random();
 
+
         for (int i = 0; i<50; i++){
             System.out.println(random.nextInt(4));
         }
